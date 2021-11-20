adc71e1beb0d44238d3e5fbb3842edabb18831f8
skrega <andrievichregina@gmail.com>
2021-11-20 12:16:08 +1000

    added remove and webpack build project

--- server/handler.js
+++ server/handler.js
@@ -1,13 +1,14 @@
-const cart = require('./cart');
 const fs = require('fs');
+const cart = require('./cart');
 
 const actions = {
     add: cart.add,
-    change: cart.change
+    change: cart.change,
+    remove: cart.remove,
 };
-//HANDLER отвечает за изменение данных в самом файле
-let handler = (req, res, action, file) => {
-    fs.readFile(file, 'utf-8', (err, data) => {
+
+const handler = (req, res, action, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
         if (err) {
             res.sendStatus(404, JSON.stringify({
                 result: 0,
@@ -17,16 +18,17 @@ let handler = (req, res, action, file) => {
             let newCart = actions[action](JSON.parse(data), req);
             fs.writeFile(file, newCart, (err) => {
                 if (err) {
-                    res.sendStatus(404, JSON.stringify({
+                    res.send(JSON.stringify({
                         result: 0,
                         text: err
                     }));
                 } else {
                     res.send(JSON.stringify({
-                        result: 1
-                    }))
+                        result: 1,
+                        text: 'SUCCESS'
+                    }));
                 }
-            })
+            });
         }
     })
 };
